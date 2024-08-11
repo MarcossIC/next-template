9ddf01d5e51a9506712b0d5e7d7c9bd979cc1717
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-08-11 14:56:55 -0300

    feat: add storybook custom default config

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+
+const config: StorybookConfig = {
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [
+		'@storybook/addon-onboarding',
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@chromatic-com/storybook',
+		'@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+	],
+	framework: {
+		name: '@storybook/nextjs',
+		options: {},
+	},
+	staticDirs: ['..\\public'],
+  features: {
+    experimentalRSC: true,
+  },
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,31 @@
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+	parameters: {
+    a11y: {
+      element: '#storybook-root',
+      config: {
+        rules: [
+          {
+            id: 'autocomplete-valid',
+            selector: '*:not([autocomplete="nope"])',
+          },
+          {
+            id: 'image-alt',
+            enabled: true,
+          },
+        ],
+      },
+      options: {},
+      manual: true,
+    },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export default preview;
