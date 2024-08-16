47cbc5dc2df4184aa4da633d977c0c5324cac56f
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-08-15 21:26:53 -0300

    refactor: change files for support type modules

--- setup-husky.js
+++ setup-husky.js
@@ -1,6 +1,10 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 try {
   // Initialize Husky
