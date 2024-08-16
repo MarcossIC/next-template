6dbe87be8d648782539e501f3b651ad8020bc20f
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-08-15 23:17:18 -0300

    feat: eslint,prettier update and run fix,write

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, type ButtonHTMLAttributes } from 'react';
+import { type ButtonHTMLAttributes, forwardRef } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	'data-testid'?: string;
