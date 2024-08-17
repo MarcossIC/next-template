1524f91f4635d6d6a50cf30ce694aac686bc1636
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-08-17 15:27:02 -0300

    chore: add jiti validation for t3-env

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,12 @@
 import withBundleAnalyzer from "@next/bundle-analyzer"
 import withPlugins from "next-compose-plugins"
 import { env } from "./env.mjs"
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./env");
 
 /** @type {import('next').NextConfig} */
 const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
