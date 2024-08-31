0d286b5664e5e8f23f330750812838abf91d0eb5
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-08-31 15:39:50 -0300

    feat: create styles directory and add path alias

---
+++ src/styles/font.ts
@@ -0,0 +1,8 @@
+import { Open_Sans } from 'next/font/google';
+
+export const openSans = Open_Sans({
+  subsets: ['latin'],
+  variable: '--font-open-sans',
+  style: ['normal'],
+  weight: ['400', '500', '600', '700'],
+});
